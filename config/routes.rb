e038b5a9616b0b00164df8bdcd456a90d775860e
Tresor <tresor.moise2001@gmail.com>
2022-01-05 11:24:30 +0200

    add device gem to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users , :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout' , :sign_up => 'register'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   resources :groups
   resources :transactions
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
   get '/external',   to: 'transactions#external'
-
-  root to: 'sessions#index'
 end
