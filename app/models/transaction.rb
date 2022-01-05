c70a369a78836778ab57f9cc6109149bb5146a67
Tresor <tresor.moise2001@gmail.com>
2022-01-05 14:16:35 +0200

    update user factory

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,5 @@ class Transaction < ApplicationRecord
   belongs_to :group, optional: true
   belongs_to :author, class_name: 'User'
   validates :name, presence: true, length: { minimum: 2, maximum: 10 }
-  validates :amount, presence: true
-  validates :author_id, presence: true
+  validates :amount , :author_id, presence: true
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
   factory :user do
     name { 'You' }
+    email { Faker::Internet.email }
+    password { 'password' }
   end
 
   factory :random_user, class: User do
     name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { 'password' }
   end
 end
