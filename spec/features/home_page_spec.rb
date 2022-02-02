c59736764f58a1ce8632e73e59de62ce8d25b132
Tresor <tresor.moise2001@gmail.com>
2022-02-02 13:03:58 +0200

    update rails version

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe 'visiting the home page', type: :feature do
   scenario 'visiting the home page' do
     visit root_path
     expect(page).to have_content('Welcome')
+    expect(page).to have_content('Sign Up')
+    expect(page).to have_content('Login')
   end
 end
